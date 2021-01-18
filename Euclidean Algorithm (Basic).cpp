848bd9997f61d0958ae8ef4b4b05d1f3aa71e9a3
TARANG0503 <40140984+TARANG0503@users.noreply.github.com>
2021-01-18 23:41:18 +0530

    Create Euclidean Algorithm (Basic).cpp

---
+++ Euclidean Algorithm (Basic).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a,int b)
+{
+  if(a==0)
+    return b;
+  return gcd(b%a,a);
+}
+int main() {
+  cout<<"Enter two numbers\n";
+  int a,b;
+  cin>>a>>b;
+  cout<<gcd(a,b);
+}
