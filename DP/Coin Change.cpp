637b2720db7cd21b5d95c580ee7dc15be5d5f15a
TARANG0503 <40140984+TARANG0503@users.noreply.github.com>
2021-01-30 00:06:58 +0530

    Update Coin Change.cpp

--- DP/Coin Change.cpp
+++ DP/Coin Change.cpp
@@ -13,17 +13,20 @@ int CoinChange(int *arr,int n,int amt)
         for(int j=1;j<=amt;j++)
         {
             if(arr[i-1]<=j)
-                dp[i][j]=1+min(dp[i-1][j],dp[i-1][j-arr[i-1]]);
+                dp[i][j]=1+min(dp[i-1][j],dp[i][j-arr[i-1]]);
             else
                 dp[i][j]=dp[i-1][j];
         }
     for(int i=0;i<=n;i++)
     {
         for(int j=0;j<=amt;j++)
-            cout<<dp[i][j]<<" ";
+            if(dp[i][j]>=1000)
+                cout<<"∞ ";
+            else
+                cout<<dp[i][j]<<" ";
         cout<<endl;
     }
-    if(dp[n][amt]==1000)
+    if(dp[n][amt]>=1000)
         return -1;
     return dp[n][amt];
 }
